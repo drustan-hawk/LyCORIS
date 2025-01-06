0e2df59b17802595b087daff5c91ddd6e1c989fb
Drustan Hawk <drustan-hawk@users.noreply.github.com>
2025-01-06 08:43:45 +0100

    Add fix for kohya module as well

--- lycoris/kohya.py
+++ lycoris/kohya.py
@@ -705,7 +705,7 @@ class LycorisNetworkKohya(LycorisNetwork):
             norms.append(norm)
             key_scaled += scaled
 
-        if key_scaled == 0:
+        if not norms:
             return 0, 0, 0
 
         return key_scaled, sum(norms) / len(norms), max(norms)
